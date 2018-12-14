266fa1f48aa306b8a348f18b138a5e815473003c
Ian Wold <ilwold@hotmail.com>
2018-12-14 14:20:25 -0600

    Update to Metalsharp nuget package

--- Metalsharp.FluidTemplate/FluidTemplate.cs
+++ Metalsharp.FluidTemplate/FluidTemplate.cs
@@ -5,30 +5,47 @@ namespace Metalsharp.FluidTemplate
 {
     public class FluidTemplate : IMetalsharpPlugin
     {
-        public void Execute(MetalsharpDirectory directory)
+        private string _layoutPath;
+
+        public FluidTemplate() =>
+            _layoutPath = null;
+
+        public FluidTemplate(string layoutPath) =>
+            _layoutPath = layoutPath;
+
+        public void Execute(MetalsharpProject project)
         {
-            foreach (var file in directory.OutputFiles.Where(i => i.Extension == "html"))
+            foreach (var file in project.OutputFiles.Where(i => i.Extension == ".html"))
             {
+                var context = new TemplateContext();
+
+                context.MemberAccessStrategy.Register<string>();
+                context.SetValue("content", file.Text);
+
+                foreach (var meta in file.Metadata)
+                {
+                    var valueType = meta.Value.GetType();
+                    valueType.GetGenericArguments().ToList().ForEach(t => context.MemberAccessStrategy.Register(t));
+
+                    context.MemberAccessStrategy.Register(valueType);
+                    context.SetValue(meta.Key, meta.Value);
+                }
+
                 if (file.Metadata.TryGetValue("template", out var _templatePath)
                     && _templatePath is string templatePath
-                    && directory.InputFiles.FirstOrDefault(i => i.FilePath == templatePath) is MetalsharpFile templateFile)
+                    && project.InputFiles.FirstOrDefault(i => i.FilePath == templatePath) is IMetalsharpFile templateFile
+                    && Fluid.FluidTemplate.TryParse(templateFile.Text, out Fluid.FluidTemplate template))
                 {
-                    if (Fluid.FluidTemplate.TryParse(templateFile.Text, out Fluid.FluidTemplate template))
-                    {
-                        var context = new TemplateContext();
+                    file.Text = template.Render(context);
+                }
 
-                        context.MemberAccessStrategy.Register<string>();
-                        context.SetValue("content", file.Text);
+                if (!string.IsNullOrEmpty(_layoutPath)
+                    && project.InputFiles.FirstOrDefault(i => i.FilePath == _layoutPath) is IMetalsharpFile layoutFile
+                    && Fluid.FluidTemplate.TryParse(layoutFile.Text, out Fluid.FluidTemplate layout))
+                {
+                    context.SetValue("content", file.Text);
 
-                        foreach (var meta in file.Metadata)
-                        {
-                            var valueType = meta.Value.GetType();
-                            context.MemberAccessStrategy.Register(valueType);
-                            context.SetValue(meta.Key, meta.Value);
-                        }
-
-                        file.Text = template.Render(context);
-                    }
+                    file.Text = layout.Render(context);
                 }
             }
         }
--- Metalsharp.FluidTemplate/MetalsharpExtension.cs
+++ Metalsharp.FluidTemplate/MetalsharpExtension.cs
@@ -2,7 +2,10 @@
 {
     public static class MetalsharpExtension
     {
-        public static MetalsharpDirectory UseFluidTemplate(this MetalsharpDirectory directory) =>
-            directory.Use(new FluidTemplate());
+        public static MetalsharpProject UseFluidTemplate(this MetalsharpProject project) =>
+            project.Use(new FluidTemplate());
+
+        public static MetalsharpProject UseFluidTemplate(this MetalsharpProject project, string layoutFile) =>
+            project.Use(new FluidTemplate(layoutFile));
     }
 }
